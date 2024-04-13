15b21a7b29b91a167d78e790645b5e2d7f075800
Shridhar-dev <shridharkamat10@gmail.com>
2024-04-13 10:57:27 +0530

    feat: agents

--- src/agents/rockets/booking_protocol.py
+++ src/agents/rockets/booking_protocol.py
@@ -1,33 +1,25 @@
-# Import necessary modules and classes from external libraries
+### Write code for the new module here and import it from agent.py.
 from ai_engine import UAgentResponse, UAgentResponseType, BookingRequest
-from uagents import Context, Protocol
+from uagents import Protocol, Context
 
-# Define a function to create a booking protocol
 def booking_proto():
-    # Create a new protocol instance
     protocol = Protocol("BookingProtocol")
 
-    # Define behavior when receiving a booking request message
     @protocol.on_message(model=BookingRequest, replies={UAgentResponse})
     async def booking_handler(ctx: Context, sender: str, msg: BookingRequest):
-        # Log the booking request
         ctx.logger.info(f"Received booking request from {sender}")
         try:
-            # Retrieve the options from context storage using the request ID
             option = ctx.storage.get(msg.request_id)
-            # Store the chosen option in context storage
             ctx.storage.set("choosen_options", option[msg.user_response])
-            # Send a response acknowledging the chosen option
             await ctx.send(
                 sender,
                 UAgentResponse(
-                    message=f"Thanks for choosing an option - {option[msg.user_response]}.",
+                    message=f"Thanks for choosing an option - {option[msg.user_response]} and weight={option['weight']}",
                     type=UAgentResponseType.FINAL,
                     request_id=msg.request_id
                 )
             )
         except Exception as exc:
-            # Handle any exceptions and send an error response
             ctx.logger.error(exc)
             await ctx.send(
                 sender,
@@ -37,4 +29,4 @@ def booking_proto():
                 )
             )
 
-    return protocol  # Return the created protocol
+    return protocol
--- src/agents/spacetrip.py
+++ src/agents/spacetrip.py
@@ -1,44 +1,63 @@
-# Import necessary modules and classes from external libraries
 from pydantic import Field
 from ai_engine import KeyValue, UAgentResponse, UAgentResponseType
-import requests
 from uagents import Agent, Protocol, Context, Model
+import requests
+import uuid
 
-# Create an agent instance for the space trip functionality
 space_trip_agent = Agent(name="Space Trip Agent", seed="50902090")
 
-# Define a custom protocol for the space trip functionality
 space_trip_protocol = Protocol("SpaceTrip")
 
-# Define a data model for the rocket
 class Rocket(Model):
     weight: str = Field(message="total weight of the person")
 
-# Define a data model for the space trip
 class SpaceTrip(Model):
     launchpad: str = Field(message="Name of the launchpad retrieved from the subtask. DO NOT ASK THIS FROM THE USER")
     rocket: str = Field(message="Name of the rocket retrieved from the subtask. DO NOT ASK THIS FROM THE USER")
+    weight: str = Field(message="Weight of the person retrieved from the subtask. DO NOT ASK THIS FROM THE USER")
     start_date: str = Field(message="Date of the day you want to go for trip. Ask from the user using a calendar interface allowing users to enter future (format=YYYY-MM-DD)")
 
-# Function to check sky conditions on a specific date using NASA API
 def sky_condition(date):
     url = f"https://api.nasa.gov/neo/rest/v1/feed?start_date={date}&end_date={date}&api_key=po3nZGZhoOlU8J5rfeWMbOFglvhWtQsxaesWkioh"
     response = requests.get(url)
-    if(response.json()['element_count'] < 10):
+    jsonRes = response.json()
+    if(jsonRes['element_count'] < 10):
         return True
     else:
         return False
 
-# Define behavior when receiving a space trip message
+def get_payment_link(launchpad, rocket, weight):
+    data = {
+        "items": [
+            {
+                "name": rocket + " - " + launchpad,
+                "images": ["https://ideogram.ai/api/images/direct/X4G1M6GTTm-AQ7KK7pSSqw.jpg"],
+                "quantity": 1,
+                "price": int(weight)*10000
+            }
+        ],
+        "email": "martiantrip@martiantrip.com"
+    }
+
+    response = requests.post("https://pixathon2024.vercel.app/api/checkout", json=data)
+    link = response.json()['session_url']
+    return link
+
 @space_trip_protocol.on_message(model=SpaceTrip, replies={UAgentResponse})
 async def on_message(ctx: Context, sender: str, msg: SpaceTrip):
     if(sky_condition(msg.start_date)):
-        # Send a response with the order summary if sky conditions are suitable
-        await ctx.send(sender, UAgentResponse(message="Order Summary: \n"+"Launchpad: "+msg.launchpad+"\nRocket: "+msg.rocket, type=UAgentResponseType.FINAL))
+        link = await get_payment_link(msg.launchpad, msg.rocket, msg.weight)
+        await ctx.send(sender, UAgentResponse(message="Order Summary: \n"+"Launchpad: "+msg.launchpad+"\nRocket: "+msg.rocket+"\nCost: Rs."+str(int(msg.weight)*10000)+'\nPayment link: <a href="'+link+ '">Pay here</a>', type=UAgentResponseType.FINAL))
     else:
-        # Send a warning along with the order summary if sky conditions are not suitable
-        await ctx.send(sender, UAgentResponse(message="Order Summary: \n"+"Launchpad: "+msg.launchpad+"\nRocket: "+msg.rocket+ "\nWarning: Too many interferences were found on the selected date. Do your thing at your own risk", type=UAgentResponseType.FINAL))
+        link = await get_payment_link(msg.launchpad, msg.rocket, msg.weight)
+        await ctx.send(sender, UAgentResponse(message="Order Summary: \n"+"Launchpad: "+msg.launchpad+"\nRocket: "+msg.rocket+"\nCost: Rs."+str(int(msg.weight)*10000)+'\nPayment link: <a href="'+link+'">Pay here</a>'+"\n\nWarning: Too many interferences(asteriods) were found on the selected date. Do your thing at your own risk", type=UAgentResponseType.FINAL))
 
-# Include the protocol in the agent and run the agent
+
+'''
+@space_trip_protocol.on_message(model=Rocket, replies={UAgentResponse})
+async def on_message(ctx: Context, sender: str, msg: Rocket):
+    # ctx.storage.set("weight",msg.weight)
+                   '''
+
 space_trip_agent.include(space_trip_protocol)
 space_trip_agent.run()
